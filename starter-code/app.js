265e9f1e3ad94cb225da8631ca714c0bbd92b551
szucsmate <szucsmate096@gmail.com>
2022-04-18 12:08:41 +0200

    refactor to class based app

---
+++ starter-code/app.js
@@ -0,0 +1,15 @@
+class App {
+    hamburger = document.querySelector('.open-menu');
+    navMenu = document.querySelector('.navigation-menu');
+    constructor() {
+        this.hamburger.addEventListener('click', this.openMenu)
+    }
+    openMenu = (e) => {
+        e.target.attributes.src.nodeValue === './assets/shared/mobile/icon-close.svg' ? e.target.attributes.src.nodeValue = './assets/shared/mobile/icon-hamburger.svg' : e.target.attributes.src.nodeValue = './assets/shared/mobile/icon-close.svg'
+        this.navMenu.style.display === 'flex' ? this.navMenu.style.display = 'none' : this.navMenu.style.display = 'flex';
+
+    }
+}
+
+
+const app = new App();
