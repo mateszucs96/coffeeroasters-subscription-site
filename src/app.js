a673f683335165c827af4b0575852b8674767895
szucsmate <szucsmate096@gmail.com>
2022-04-28 11:58:48 +0200

    add comments for better reading experiment

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ class Plan {
 }
 
 class App {
+    //selectors
     hamburger = document.querySelector('.hamburger');
     navMenu = document.querySelector('.navigation-menu');
     accordionHeader = document.querySelectorAll('.accordion__header');
@@ -39,25 +40,35 @@ class App {
 
     };
 
+    //update summary section
     updateSummary(arr) {
+        // update summary textcontent based on the choosen answer
         this.placesholders.forEach((el, i) => {
             el.textContent = arr[i];
 
         });
+        // if capsule is choosen...
         if (arr[0] === 'Capsule') {
+            // ...'using' is before the first placeholder 
             this.how.textContent = 'using';
+            //... 'Want us to grind them?' accordion disabled
             this.accordionHeader[3].children[0].style.color = '#C1C3C7';
             this.accordionHeader[3].removeEventListener('click', this.handleAccordion);
 
         }
+        // if others is choosen... 
         if (arr[0] === 'Filter' || arr[0] === 'Espresso') {
+            // ... 'as' is before the first placeholder
+            this.how.textContent = 'as';
+            //... 'Want us to grind them?' accordion enabled
             this.accordionHeader[3].addEventListener('click', this.handleAccordion)
             this.accordionHeader[3].children[0].style.color = '#83888F';
-            this.how.textContent = 'as';
+
         }
 
     };
 
+    // handling answers based on selection 
     handleAnswers(e) {
 
 
@@ -66,19 +77,28 @@ class App {
             Array.from(e.target.closest('.answer').parentNode.children).forEach(el => el.style.backgroundColor = '');
             // add bg
             e.target.closest('.answer').style.backgroundColor = 'red';
+            // selected answer
             const clicked = e.target.closest('.answer').parentNode.classList[1];
+            // pass it to the 'sub' object
             sub[clicked] = e.target.closest('.answer').children[0].textContent.trim();
         };
+        // create 'plan' instance based on 'sub' object
         const plan = new Plan(sub.pref, sub.type, sub.quantity, sub.grind, sub.deliver);
 
+        // update summary section from 'plan'
         this.updateSummary(Object.values(plan));
 
     };
 
+    // accordion functionality
     handleAccordion(e) {
+        // clicked accordion section
         const clickedContainer = e.target.closest('.accordion');
+
         const arrowIcon = clickedContainer.children[0].children[1];
+        // rotate arrow icon
         arrowIcon.classList.contains('rotate') ? arrowIcon.classList.remove('rotate') : arrowIcon.classList.add('rotate')
+        // show / hide accordion body
         if (clickedContainer.children[1].style.display === "" || clickedContainer.children[1].style.display === "none") {
             clickedContainer.children[1].style.display = 'flex';
         } else {
@@ -86,9 +106,4 @@ class App {
         };
     };
 }
-
-
-
-
-
 const app = new App();
