7a26c0f237cef25d082fbab01eac7e4b38753699
szucsmate <szucsmate096@gmail.com>
2022-05-12 19:05:48 +0200

    test hosting

---
+++ src/navigation.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navigation-menu');
+
+
+
+
+const openMenu = (e) => {
+    e.target.attributes.src.nodeValue === './assets/shared/mobile/icon-close.svg'
+        ? e.target.attributes.src.nodeValue = './assets/shared/mobile/icon-hamburger.svg'
+        : e.target.attributes.src.nodeValue = './assets/shared/mobile/icon-close.svg';
+    navMenu.style.display === 'flex' ? navMenu.style.display = 'none' : navMenu.style.display = 'flex';
+
+};
+
+hamburger.addEventListener('click', openMenu)
